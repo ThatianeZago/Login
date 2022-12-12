f96c2172651d639baa007c0e5f7d67edbc1a8364
Thatiane Leite <thatianelzago@gmail.com>
2022-12-12 15:12:24 +0000

    Initial commit

---
+++ app/src/main/java/com/example/login/MostraUtilizadores.java
@@ -0,0 +1,14 @@
+package com.example.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MostraUtilizadores extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mostra_utilizadores);
+    }
+}
